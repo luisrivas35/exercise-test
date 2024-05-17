2bb79c3848e1692b11accc7a0f107dfd688f8174
Luis Rivas <xluisrivas@gmail.com>
2024-05-17 19:07:17 -0400

    upload code

---
+++ database/connection.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import pg from 'pg'
+const { Pool } = pg;
+const connectionString = process.env.POSTGRES_URL;
+
+export const pool = new Pool({
+  connectionString,
+  allowExitOnIdle: true,
+});
+
+try {
+    const time = await pool.query('SELECT NOW()')
+    console.log('db conectada!' + time.rows[0].now)
+} catch (error) {
+    console.log(error)
+}
